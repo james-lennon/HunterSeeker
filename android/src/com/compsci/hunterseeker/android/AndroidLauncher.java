c8cb10a8ebcf301dc06a17fc3790d083a6444157
James Lennon <jameslennon321@gmail.com>
2015-04-14 15:35:51 -0700

    Initial commit

---
+++ android/src/com/compsci/hunterseeker/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.compsci.hunterseeker.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.compsci.hunterseeker.HunterSeeker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HunterSeeker(), config);
+	}
+}
